36c98dd56371e2addfd0d03adb049979ea207d2e
michal-kusnierz <kusnierz.michal@gmail.com>
2019-12-30 18:26:25 +0100

    deleted semicolons and used single quotes

--- my-app-graphql/src/index.js
+++ my-app-graphql/src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/index.css";
-import App from "./components/App";
-import { Provider, Client, defaultExchanges } from "urql";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import { Provider, Client, defaultExchanges } from 'urql'
 
 const client = new Client({
-  url: "http://localhost:4000",
+  url: 'http://localhost:4000',
   exchanges: defaultExchanges
-});
+})
 
 ReactDOM.render(
   <Provider value={client}>
     <App />
   </Provider>,
   document.getElementById('root')
-);
+)
