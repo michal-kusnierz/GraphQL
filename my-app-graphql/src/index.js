a87cf8888de121f3b139061b58915ca5849561f8
michal-kusnierz <kusnierz.michal@gmail.com>
2019-12-30 18:23:27 +0100

    client instance added

--- my-app-graphql/src/index.js
+++ my-app-graphql/src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import { Provider, Client, defaultExchanges } from 'urql'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import { Provider, Client, defaultExchanges } from "urql";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new Client({
+  url: "http://localhost:4000",
+  exchanges: defaultExchanges
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
