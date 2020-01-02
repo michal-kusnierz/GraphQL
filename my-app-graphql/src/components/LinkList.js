3dcb13f715482f6fb4cb3f773a8b849096a96315
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-02 19:42:08 +0100

    initial object added in LinkList

--- my-app-graphql/src/components/LinkList.js
+++ my-app-graphql/src/components/LinkList.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import Link from './Link'
 
-const Link = ({ link }) => (
+const linksToRender = [
+  {
+    id: '1',
+    description: 'Prisma turns your database into a GraphQL API 😎',
+    url: 'https://www.prismagraphql.com',
+  },
+  {
+    id: '2',
+    description: 'The best GraphQL client',
+    url: 'https://formidable.com/open-source/urql/',
+  },
+]
+
+const LinkList = () => (
   <div>
-    <div>
-      {link.description} ({link.url})
-    </div>
+    {linksToRender.map(link => <Link key={link.id} link={link} />)}
   </div>
 )
 
-export default Link
+export default LinkList
