6549f2341f68ac6dcb0441ac9f65c8f89f1f130a
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-02 19:42:27 +0100

    App component renders LinkList

--- my-app-graphql/src/components/App.js
+++ my-app-graphql/src/components/App.js
@@ -1,36 +1,6 @@
-import React from 'react';
-import logo from '../../src/logo.svg';
-import '../styles/App.css';
+import React from 'react'
+import LinkList from './LinkList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          1. get to know GraphQL
-        </p>
-        <a
-          className="App-link"
-          href="https://graphql.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn GraphQL
-        </a>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>2. maybe use Prisma...?</p>
-        <a
-          className="App-link"
-          href="https://www.prisma.io/with-graphql"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get started with GraphQL & Prisma
-        </a>
-
-      </header>
-    </div>
-  );
-}
+const App = () => <LinkList />
 
 export default App;
