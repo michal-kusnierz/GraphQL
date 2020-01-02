a28fa1863ca0bc23c006e3b2289deb7a79b10b81
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-03 00:10:22 +0100

    added Header and routing to App.js

--- my-app-graphql/src/components/App.js
+++ my-app-graphql/src/components/App.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Header from './Header'
+import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 
-const App = () => <CreateLink />
+const App = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route exact path="/" component={LinkList} />
+        <Route exact path="/create" component={CreateLink} />
+      </Switch>
+    </div>
+  </div>
+)
 
 export default App;
