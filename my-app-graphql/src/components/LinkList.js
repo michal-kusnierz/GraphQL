69d244c2b9097e371a994d964f9244c298cd2368
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-02 20:31:16 +0100

    rearranged LinkList func

--- my-app-graphql/src/components/LinkList.js
+++ my-app-graphql/src/components/LinkList.js
@@ -17,13 +17,19 @@ const FEED_QUERY = gql`
 `
 
 const LinkList = () => {
-  useQuery({ query: FEED_QUERY });
+  const [result] = useQuery({ query: FEED_QUERY })
+  const { data, fetching, error } = result
+  
+  if (fetching) return <div>Fetching</div>
+  if (error) return <div>Error</div>
+  
+  const linksToRender = data.feed.links
 
   return (
     <div>
       {linksToRender.map(link => <Link key={link.id} link={link} />)}
     </div>
-  );
-};
+  )
+}
 
 export default LinkList
