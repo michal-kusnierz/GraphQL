fe606a9c0961f4bc07e818d85974ad1b6cc74066
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-04 00:49:57 +0100

    GraphQL mutations added to Login component

--- my-app-graphql/src/components/Login.js
+++ my-app-graphql/src/components/Login.js
@@ -1,5 +1,23 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import { useMutation } from 'urql'
 import { setToken } from '../token'
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`
 
 const Login = props => {
 
