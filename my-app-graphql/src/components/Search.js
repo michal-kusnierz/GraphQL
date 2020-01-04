d8a99092b772bc446c9d7005921e9c40a333bc16
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-04 14:46:04 +0100

    query definition added with imports from urql

--- my-app-graphql/src/components/Search.js
+++ my-app-graphql/src/components/Search.js
@@ -3,6 +3,29 @@ import { useQuery } from 'urql'
 import gql from 'graphql-tag'
 import Link from './Link'
 
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
 const Search = () => {
   const [filter, setFilter] = React.useState('')
 
