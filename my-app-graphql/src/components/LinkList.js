0cc2f3c9c2a97e0076dd2ee99d9615f77d7c2d03
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-07 13:46:59 +0100

    linksToRender modified

--- my-app-graphql/src/components/LinkList.js
+++ my-app-graphql/src/components/LinkList.js
@@ -40,23 +40,35 @@ const LinkList = props => {
   const [result] = useQuery({ query: FEED_QUERY, variables })
   const { data, fetching, error } = result
   
-  if (fetching) return <div>Fetching</div>
-  if (error) return <div>Error</div>
+  const linksToRender = React.useMemo(() => {
+    if (!data) {
+      return [];
+    } else if (isNewPage) {
+      return data.feed.links;
+    } else {
+      const rankedLinks = data.feed.links
+        .slice()
+        .sort((l1, l2) => l2.votes.length - l1.votes.length);
+      return rankedLinks;
+    }
+  }, [data, isNewPage]);
   
-  const linksToRender = data.feed.links
-  const pageIndex = isNewPage ? (page - 1) * 10 : 0
-
   const nextPage = React.useCallback(() => {
     if (page <= data.feed.count / 10) {
       props.history.push(`/new/${page + 1}`);
     }
-  }, [props.history, data.feed.count, page]);
+  }, [props.history, data, page]);
 
   const previousPage = React.useCallback(() => {
     if (page > 1) {
       props.history.push(`/new/${page - 1}`);
     }
   }, [props.history, page]);
+
+  if (fetching) return <div>Fetching</div>
+  if (error) return <div>Error</div>
+  
+  const pageIndex = isNewPage ? (page - 1) * 10 : 0
 
   return (
     <React.Fragment>
