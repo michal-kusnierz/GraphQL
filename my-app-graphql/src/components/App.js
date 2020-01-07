24db92f595f4fc3d06e8650585a4a9a34b7f2091
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-07 12:22:57 +0100

    Redirect and LoadingBoundary added to App.js

--- my-app-graphql/src/components/App.js
+++ my-app-graphql/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import LoadingBoundary from './LoadingBoundary'
 import Header from './Header'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
@@ -10,12 +11,16 @@ const App = () => (
   <div className="center w85">
     <Header />
     <div className="ph3 pv1 background-gray">
-      <Switch>
-        <Route exact path="/" component={LinkList} />
-        <Route exact path="/create" component={CreateLink} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/search" component={Search} />
-      </Switch>
+      <LoadingBoundary>
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+          <Route exact path="/top" component={LinkList} />
+          <Route exact path="/new/:page" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
+        </Switch>
+      </LoadingBoundary>
     </div>
   </div>
 )
