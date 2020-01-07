481cc86017746cc84a628d37f637741b4972c06a
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-07 12:52:29 +0100

    removed LoadingBoundary as it's not used

--- my-app-graphql/src/components/App.js
+++ my-app-graphql/src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import LoadingBoundary from './LoadingBoundary'
 import Header from './Header'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
@@ -11,16 +10,14 @@ const App = () => (
   <div className="center w85">
     <Header />
     <div className="ph3 pv1 background-gray">
-      <LoadingBoundary>
-        <Switch>
-          <Route exact path="/" render={() => <Redirect to="/new/1" />} />
-          <Route exact path="/top" component={LinkList} />
-          <Route exact path="/new/:page" component={LinkList} />
-          <Route exact path="/create" component={CreateLink} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/search" component={Search} />
-        </Switch>
-      </LoadingBoundary>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+        <Route exact path="/top" component={LinkList} />
+        <Route exact path="/new/:page" component={LinkList} />
+        <Route exact path="/create" component={CreateLink} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/search" component={Search} />
+      </Switch>
     </div>
   </div>
 )
