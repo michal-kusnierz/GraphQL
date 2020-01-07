eaaf853f405dbab11770a4a4ceba7933e8d5b713
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-07 20:25:37 +0100

    fixes in index.js; with beautify

--- my-app-graphql/src/index.js
+++ my-app-graphql/src/index.js
@@ -1,49 +1,60 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import './styles/index.css'
-import App from './components/App'
-import { Provider, Client, dedupExchange, fetchExchange, subscriptionExchange } from 'urql'
-import { SubscriptionClient } from 'subscription-transport-ws'
-import { cacheExchange } from '@urql/exchange-graphcache'
-import { getToken } from './token'
-import { FEED_QUERY } from './components/LinkList'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter } from 'react-router-dom';
+import { SubscriptionClient } from "subscriptions-transport-ws";
+
+import {
+  Provider,
+  createClient,
+  fetchExchange,
+  dedupExchange,
+  subscriptionExchange
+} from "urql";
+
+import { cacheExchange } from '@urql/exchange-graphcache';
+
+import "./styles/index.css";
+
+import App from './components/App';
+import { FEED_QUERY } from './components/LinkList';
+import { getToken } from './token';
 
 const cache = cacheExchange({
   updates: {
     Mutation: {
       post: ({ post }, _args, cache) => {
-        const variables = { first: 10, skip: 0, orderBy: 'createdAt_DESC' }
+        const variables = { first: 10, skip: 0, orderBy: "createdAt_DESC" };
         cache.updateQuery({ query: FEED_QUERY, variables }, data => {
           if (data !== null) {
-            data.feed.links.unshift(post)
-            data.feed.count++
-            return data
+            data.feed.links.unshift(post);
+            data.feed.count++;
+            return data;
           } else {
-            return null
+            return null;
           }
-        })
+        });
       }
     },
     Subscription: {
       newLink: ({ newLink }, _args, cache) => {
-        const variables = { first: 10, skip: 0, orderBy: 'createdAt_DESC' }
+        const variables = { first: 10, skip: 0, orderBy: "createdAt_DESC" };
         cache.updateQuery({ query: FEED_QUERY, variables }, data => {
           if (data !== null) {
-            data.feed.links.unshift(newLink)
-            data.feed.count++
-            return data
+            data.feed.links.unshift(newLink);
+            data.feed.count++;
+            return data;
           } else {
-            return null
+            return null;
           }
-        })
+        });
       }
     }
   }
-})
+});
 
 const subscriptionClient = new SubscriptionClient(
-  "ws://localhost:4000",
+  'ws://localhost:4000',
   {
     reconnect: true,
     connectionParams: {
@@ -52,29 +63,29 @@ const subscriptionClient = new SubscriptionClient(
   }
 );
 
-const client = new Client({
+const client = createClient({
   url: 'http://localhost:4000',
   fetchOptions: () => {
-    const token = getToken()
+    const token = getToken();
     return {
       headers: { authorization: token ? `Bearer ${token}` : '' }
     }
   },
   exchanges: [
-    dedupExchange, 
-    cache, 
+    dedupExchange,
+    cache,
     fetchExchange,
     subscriptionExchange({
       forwardSubscription: operation => subscriptionClient.request(operation)
     })
-  ],
-})
+  ]
+});
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider value={client}>
       <App />
     </Provider>
-    </BrowserRouter>,
-  document.getElementById('root')
-)
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
