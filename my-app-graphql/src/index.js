91221f046b57672a9ad6b041bcaedc1bbd45371b
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-07 13:51:37 +0100

    cacheExchange updated with feed_query import

--- my-app-graphql/src/index.js
+++ my-app-graphql/src/index.js
@@ -6,8 +6,26 @@ import App from './components/App'
 import { Provider, Client, dedupExchange, fetchExchange } from 'urql'
 import { cacheExchange } from '@urql/exchange-graphcache'
 import { getToken } from './token'
+import { FEED_QUERY } from './components/LinkList'
 
-const cache = cacheExchange({})
+const cache = cacheExchange({
+  updates: {
+    Mutation: {
+      post: ({ post }, _args, cache) => {
+        const variables = { first: 10, skip: 0, orderBy: 'createdAt_DESC' }
+        cache.updateQuery({ query: FEED_QUERY, variables }, data => {
+          if (data !== null) {
+            data.feed.links.unshift(post)
+            data.feed.count++
+            return data
+          } else {
+            return null
+          }
+        })
+      }
+    }
+  }
+})
 
 const client = new Client({
   url: 'http://localhost:4000',
