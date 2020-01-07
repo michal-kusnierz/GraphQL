35e548b8f1140cc5b57ccde8d15b1a8a4cb25272
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-07 12:35:25 +0100

    added new link to Header component routing

--- my-app-graphql/src/components/Header.js
+++ my-app-graphql/src/components/Header.js
@@ -13,7 +13,11 @@ const Header = props => {
         <Link to="/" className="ml1 no-underline black">
           new
         </Link>
-          <div className="ml1">|</div>
+        <div className="ml1">|</div>
+        <Link to="/top" className="ml1 no-underline black">
+          top
+        </Link>
+        <div className="ml1">|</div>
         <Link to="/search" className="ml1 no-underline black">
           search
         </Link>
