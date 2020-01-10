a7d6526162ba659261926b809f3c47692e4e71a0
michal-kusnierz <kusnierz.michal@gmail.com>
2020-01-10 18:11:24 +0100

    prisma fix: endpoint added to index.js

--- my-app-graphql/server/src/generated/prisma-client/index.js
+++ my-app-graphql/server/src/generated/prisma-client/index.js
@@ -20,7 +20,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: ``,
-  secret: `mysecret123`
+  endpoint: `https://eu1.prisma.sh/michal-kusnierz-1bdcac/my-app-graphql/graphql`
 });
 exports.prisma = new exports.Prisma();
